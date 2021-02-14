de09aff340eb36b5e6861b2954db4d00691b25dd
wwt0805 <58569651+wwt0805@users.noreply.github.com>
2021-02-14 09:27:35 +0800

    Update p05_prisons.py

--- p05_prisons.py
+++ p05_prisons.py
@@ -1,36 +1,42 @@
-'''
+"""
 coding=utf-8
 Author Wu Wentong
 @Time    : 2021/2/6 4:14 下午
-@Site    : 
+@Site    :
 @File    : p05_prisons.py
 @Software: PyCharm
-'''
+"""
 import random
 
-def solve_prisons(num):
-    counter = num - 1
-    turn_ons = [False] * (num - 1)  # 状态列表，用于记录状态
-    lamp = False
-    count = 0
 
-    while True:
-        lucky = random.randint(0, num - 1)
-        lamp, count = get_free(lucky, counter, turn_ons, lamp, count)
-        if count == num - 1:
-            break
+class PrisonersEscape:
+    def __init__(self, num):
+        self.num = num
 
-def get_free(lucky, counter, turn_ons, lamp, count):
-    if lucky == counter:
-        if lamp:
-            lamp = False
-            count += 1
-    else:  # 对于非计数员
-        if not lamp and not turn_ons[lucky]:
-            lamp = True
-            turn_ons[lucky] = True
-    print('luck={}, lamp={}, count={}, {}'.format(lucky, lamp, count, turn_ons))
-    return lamp, count
+    def solve_prisons(self):
+        counter = self.num - 1
+        turn_ons = [False] * (self.num - 1)  # 状态列表，用于记录状态
+        lamp = False
+        count = 0
+        while True:
+            lucky = random.randint(0, self.num - 1)
+            lamp, count = self.get_free(lucky, counter, turn_ons, lamp, count)
+            if count == self.num - 1:
+                break
+
+    @staticmethod
+    def get_free(lucky, counter, turn_ons, lamp, count):
+        if lucky == counter:
+            if lamp:
+                lamp = False
+                count += 1
+        else:  # 对于非计数员
+            if not lamp and not turn_ons[lucky]:
+                lamp = True
+                turn_ons[lucky] = True
+        print('luck={}, lamp={}, count={}, {}'.format(lucky, lamp, count, turn_ons))
+        return lamp, count
 
 
-solve_prisons(4)
+if __name__ == "__main__":
+    PrisonersEscape(4).solve_prisons()
