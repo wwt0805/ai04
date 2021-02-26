e3aa62ca0cf9bffd735ae531b123d0a40f23b40e
wwt0805 <58569651+wwt0805@users.noreply.github.com>
2021-02-26 11:50:26 +0800

    2/25 p22更新

--- p22_platform.py
+++ p22_platform.py
@@ -1,15 +1,22 @@
-
+"""
+coding=utf-8
+@Author  : Wu Wentong
+@Time    : 2021/2/26 11:36 上午
+@Site    :
+@File    : p22_platform.py
+@Software: PyCharm
+"""
 import p21_Exp as exp
 
 
-def solve(exps, lr=0.001, epoches=10000):
-    vars = set()
+def solve(exps, lr=0.01, epoches=1000):
+    vars = set()  # 变量集合
     derivs = {e: {v: e.deriv(v) for v in e.get_vars()} for e in exps}
 
     for e in derivs:
         for v in derivs[e]:
             vars.add(v)
-    vars = {v:1.0 for v in vars}
+    vars = {v: 1.0 for v in vars}
 
     for _ in range(epoches):
         for e in exps:
@@ -21,16 +28,16 @@ def solve(exps, lr=0.001, epoches=10000):
                 ds[v] = delta
             for v in vs:
                 vars[v] = vars[v] + ds[v]
-
     return vars
 
 
-v = [exp.Variable(chr(ord('a') + i)) for i in range(26)]
-exps = []
-while True:
-    s = input('expression = ?')
-    if len(s) == 0:
-        break
-    s = eval(s)
-    exps.append(s)
-print(solve(exps))
+if __name__ == "__main__":
+    v = [exp.Variable(chr(ord('a') + i)) for i in range(26)]
+    exps = []
+    while True:
+        s = input('expression = ?')
+        if len(s) == 0:
+            break
+        s = eval(s)
+        exps.append(s)
+    print(solve(exps))
