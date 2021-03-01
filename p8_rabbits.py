90066307dd351bb818efbef11ee0780fb8d7333e
wwt0805 <58569651+wwt0805@users.noreply.github.com>
2021-03-01 10:03:15 +0800

    Delete p8_rabbits.py

--- p8_rabbits.py
+++
@@ -1,8 +0,0 @@
-def get_rabbits(months):
-    if months <= 2:
-        return 1
-    return get_rabbits(months - 1) + get_rabbits(months - 2)
-
-
-for months in range(1, 50 + 1):
-    print(months, get_rabbits(months))
